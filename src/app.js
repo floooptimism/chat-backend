5e92e6e901bc350469c7b293baa05026ca2fc294
goblin <jackmord445@gmail.com>
2022-05-23 01:45:54 +0800

    express cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const { createServer }  = require('http');
 const express = require('express');
 const ChatServer = require('./server/ChatServer');
 
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+
 const server = createServer(app);
 const chatServer = new ChatServer();
 
@@ -11,7 +15,7 @@ const chatServer = new ChatServer();
 // Socket IO Server
 chatServer.start(server);
 
-server.listen(3000, () => {
+server.listen(3001, () => {
     console.log('listening on port 3000');
 });
 
