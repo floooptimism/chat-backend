6f83e5b9c4dc6ea2ab79feed90720800eac4fa2a
goblin <jackmord445@gmail.com>
2022-05-31 16:08:12 +0800

    what

--- src/server/ChatServer.js
+++ src/server/ChatServer.js
@@ -1,119 +1,135 @@
-const { Server } = require('socket.io');
-const validateToken = require('./middlewares/validateToken');
-const uid = require('uid2');
+const { Server } = require("socket.io");
+const validateToken = require("./middlewares/validateToken");
+const uid = require("uid2");
 
-function generateID(){
-    return uid(32);
+function generateID() {
+  return uid(32);
 }
 
-class ChatServer{
-    constructor(){
-        this.io = null;
-        this.events = {
-            message_to_room: [],
-            join_room: [],
-        };
+class ChatServer {
+  constructor() {
+    this.io = null;
+    this.events = {
+      message_to_room: [],
+      join_room: [],
+    };
 
-        this.users = new Map();
-        this.rooms = new Map();
+    this.users = new Map();
+    this.rooms = new Map();
+  }
 
-    }
+  addUser(id, data) {
+    this.users.set(id, data);
+  }
 
-    addUser(id, data){
-        this.users.set(id, data);
-    }
+  removeUser(id) {
+    this.users.delete(id);
+  }
 
-    removeUser(id){
-        this.users.delete(id);
-    }
+  addRoom(roomID, roomName) {
+    this.rooms.set(roomID, roomName);
+  }
 
-    addRoom(roomID, roomName){
-        this.rooms.set(roomID, roomName);
-    }
+  removeUserFromCurrentRoom(socket) {
+    let user = this.users.get(socket.id);
+    let currentRoom = user.roomID;
+    user.roomID = null;
+    this.addUser(socket.id, user);
+    socket.leave(currentRoom);
+  }
 
-    removeUserFromCurrentRoom(socket) {
-        let user = this.users.get(socket.id);
-        let currentRoom = user.roomID;
-        user.roomID = null;
-        this.addUser(socket.id, user);
-        socket.leave(currentRoom);
-    }
+  addUserToRoom(socket, roomID) {
+    let user = this.users.get(socket.id);
+    user.roomID = roomID;
+    this.addUser(socket.id, user);
+    socket.join(roomID);
+  }
 
-    addUserToRoom(socket, roomID){
-        let user = this.users.get(socket.id);
-        user.roomID = roomID;
-        this.addUser(socket.id, user);
-        socket.join(roomID);
-    }
+  start(httpServer) {
+    console.log("Starting server");
+    this.io = new Server(httpServer, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
+    let self = this;
 
+    this.io.on("connection", (socket) => {
+      // listeners
+      console.log("Someone connected.");
+      self.addUser(socket.id, {
+        username: socket.data.username,
+        profile_picture: socket.data.profile_picture,
+        roomID: null,
+      });
 
-    start(httpServer){
-        this.io = new Server(httpServer, {
-            cors: {
-                origin: "*",
-                methods: ["GET", "POST"]
-            }
+      socket.on("message_to_room", function ({ message }) {
+        let user = self.users.get(socket.id);
+        let messageTimestamp = Date.now();
+        // if(!self.rooms.get(user.roomID)) return;
+        self.io.to(user.roomID).emit("message_from_room", {
+          id: generateID() + messageTimestamp,
+          user: {
+            name: user.username,
+            profile_picture: user.profile_picture,
+          },
+          message: message,
+          timestamp: messageTimestamp,
         });
-        let self = this;
+      });
 
-        this.io.on('connection', (socket) => {
-            // listeners
-            console.log("Someone connected.");
-            self.addUser(socket.id, {username: socket.data.username, roomID: null});
+      socket.on("join_room", function ({ room }) {
+        self.removeUserFromCurrentRoom(socket);
+        self.addUserToRoom(socket, room.channelId);
+        socket.emit("join_room_success", { room });
+      });
 
-            socket.on("message_to_room", function({message}){
-                let user = self.users.get(socket.id);
-                let messageTimestamp = Date.now();
-                // if(!self.rooms.get(user.roomID)) return;
-                self.io.to(user.roomID).emit("message_from_room", {id: generateID()+messageTimestamp, user: user.username, message: message, timestamp: messageTimestamp});
-            })
+      socket.on("disconnect", () => {
+        self.removeUserFromCurrentRoom(socket);
+        self.removeUser(socket.id);
+      });
+    });
 
-            socket.on("join_room", function({room}){
-                self.removeUserFromCurrentRoom(socket);
-                self.addUserToRoom(socket, room.channelId);
-                socket.emit('join_room_success', {room});
-            })
+    //middlewares
+    this.io.use(validateToken);
 
-            socket.on("disconnect", () => {
-                self.removeUserFromCurrentRoom(socket);
-                self.removeUser(socket.id);
-            })
-        });
+    // Adapters listeners
+    this.io.sockets.adapter.on("create-room", function (room) {
+      console.log("Creating room: " + room);
+      self.io.emit("rooms_list", Array.from(self.rooms.values()));
+    });
 
-        //middlewares
-        this.io.use(validateToken);
+    this.io.sockets.adapter.on("join-room", function (room, id) {
+      console.log("Joining room: " + room);
+      let users = Array.from(self.users.values()).filter(
+        (user) => user.roomID === room
+      );
+      self.io.to(room).emit("other_user_joined_room", {
+        users: users,
+        newUser: self.users.get(id) && self.users.get(id).username,
+      });
+    });
 
-        // Adapters listeners
-        this.io.sockets.adapter.on('create-room', function(room) {
-            console.log("Creating room: " + room);
-            self.io.emit("rooms_list", Array.from(self.rooms.values()));
-        });
+    this.io.sockets.adapter.on("leave-room", function (room, IDOfUserThatLeft) {
+      console.log("Leaving room: " + room);
+      let users = Array.from(self.users.values()).filter(
+        (user) => user.roomID === room
+      );
+      self.io.to(room).emit("other_user_left_room", {
+        users: users,
+        userThatLeft:
+          self.users.get(IDOfUserThatLeft) &&
+          self.users.get(IDOfUserThatLeft).username,
+      });
+    });
+  }
 
-        this.io.sockets.adapter.on('join-room', function(room, id){
-            console.log("Joining room: " + room);
-            let users = Array.from(self.users.values()).filter(user => user.roomID === room);
-            self.io.to(room).emit("joined_room", {
-                users: users,
-                newUser: self.users.get(id) && self.users.get(id).username,
-            });
-        })
-
-        this.io.sockets.adapter.on('leave-room', function(room, IDOfUserThatLeft){
-            console.log("Leaving room: " + room);
-            let users = Array.from(self.users.values()).filter(user => user.roomID === room);
-            self.io.to(room).emit("left_room", {
-                users: users,
-                userThatLeft: self.users.get(IDOfUserThatLeft) && self.users.get(IDOfUserThatLeft).username
-            });
-        })
-    }
-
-    stop(){
-        this.io.close();
-        this.users.clear();
-        this.rooms.clear();
-    }
-
+  stop() {
+    this.io.close();
+    this.users.clear();
+    this.rooms.clear();
+  }
 }
 
 module.exports = ChatServer;
