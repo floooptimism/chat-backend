dc027b316bd26cedb2f836e7e103b9e2e21220ca
Goblin <goblin.wx7p6@aleeas.com>
2022-06-02 16:07:05 +0800

    add test rooms

--- src/app.js
+++ src/app.js
@@ -12,6 +12,20 @@ app.use(cors());
 const server = createServer(app);
 const chatServer = new ChatServer();
 
+// add test rooms
+chatServer.addRoom('testRoom', {
+    id: 'testRoom',
+    name: 'Testing Room',
+    description: 'This is a test room',
+    image: 'https://ui-avatars.com/api/?name=Testing+Room'
+})
+
+chatServer.addRoom('testRoom2', {
+    id: 'testRoom2',
+    name: "Another Room",
+    description: 'Welcome welcome welcome',
+    image: 'https://ui-avatars.com/api/?name=Another+Room'
+})
 
 // Socket IO Server
 chatServer.start(server);
